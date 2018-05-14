edf795ad613f1cb0c2b6d8670d7906fe85e523c2
Xu Sun <sunxuhit@gmail.com>
2018-05-14 16:26:28 -0400

    v0.72: update PDF plotting macro

--- macros/plotQA_PDF.C
+++ macros/plotQA_PDF.C
@@ -1,7 +1,7 @@
 #include "TFile.h"
 #include "TString.h"
 #include "TTree.h"
-#include "TH3D.h"
+#include "TH2D.h"
 #include "TCanvas.h"
 #include <cmath>
 #include <cassert>
@@ -23,50 +23,101 @@ std::pair<int,std::string> get_particle(int pid)
   else return std::pair<int,std::string>(-1,"undefined");
 }
 
-void plotQA_PDF(int pid = 211)
+void plotQA_PDF()
 {
-  auto particle = get_particle(pid);
-  cout << "particle.first = " << particle.first << ", particle.second = " << particle.second << endl;
-
-  if(particle.first == -1) return;
-
   string date = "May10_2018";
   string inputfile = Form("/work/eic/xusun/output/database/PDF_database_%s.root",date.c_str());
   cout << "read in file: " << inputfile.c_str() << endl;
   TFile *File_InPut = TFile::Open(inputfile.c_str());
-
   assert(File_InPut);
-  string histQA = Form("h_photonDist_%s",particle.second.c_str());
-  TH3D *h_PDF = (TH3D*)File_InPut->Get(histQA.c_str());
-  assert(h_PDF);
 
-  TCanvas *c_PDF = new TCanvas("c_PDF","c_PDF",10,10,800,800);
-  c_PDF->cd()->SetLeftMargin(0.15);
-  c_PDF->cd()->SetBottomMargin(0.15);
-  c_PDF->cd()->SetGrid(0,0);
-  c_PDF->cd()->SetTicks(1,1);
+  string key_pion = "h_photonDist_piplus_vx_0_vy_1_mom_1";
+  TH2D *h_PDF_pion = (TH2D*)File_InPut->Get(key_pion.c_str());
+  assert(h_PDF_pion);
 
-  h_PDF->SetTitle(particle.second.c_str());
-  h_PDF->SetStats(0);
+  string key_kaon = "h_photonDist_Kplus_vx_0_vy_1_mom_1";
+  TH2D *h_PDF_kaon = (TH2D*)File_InPut->Get(key_kaon.c_str());
+  assert(h_PDF_kaon);
 
-  h_PDF->GetXaxis()->SetTitle("out_x (mm)");
-  h_PDF->GetXaxis()->CenterTitle();
-  h_PDF->GetXaxis()->SetTitleOffset(1.25);
-  h_PDF->GetXaxis()->SetNdivisions(505);
+  string key_proton = "h_photonDist_proton_vx_0_vy_1_mom_1";
+  TH2D *h_PDF_proton = (TH2D*)File_InPut->Get(key_proton.c_str());
+  assert(h_PDF_proton);
 
-  h_PDF->GetYaxis()->SetTitle("out_y (mm)");
-  h_PDF->GetYaxis()->CenterTitle();
-  h_PDF->GetYaxis()->SetTitleOffset(1.25);
-  h_PDF->GetYaxis()->SetNdivisions(505);
+  TCanvas *c_PDF = new TCanvas("c_PDF","c_PDF",10,10,1500,500);
+  c_PDF->Divide(3,1);
+  for(int i_pad = 0; i_pad < 3; ++i_pad)
+  {
+    c_PDF->cd(i_pad+1);
+    c_PDF->cd(i_pad+1)->SetLeftMargin(0.15);
+    c_PDF->cd(i_pad+1)->SetBottomMargin(0.15);
+    c_PDF->cd(i_pad+1)->SetGrid(0,0);
+    c_PDF->cd(i_pad+1)->SetTicks(1,1);
+  }
 
-  h_PDF->GetZaxis()->SetTitle("generated momemtum (GeV/c)");
-  h_PDF->GetZaxis()->CenterTitle();
-  h_PDF->GetZaxis()->SetTitleOffset(1.25);
-  h_PDF->GetZaxis()->SetNdivisions(505);
+  c_PDF->cd(1);
+  h_PDF_pion->SetTitle("pi+ mass hypotheses");
+  h_PDF_pion->SetStats(0);
 
-  h_PDF->Draw();
+  h_PDF_pion->GetXaxis()->SetTitle("out_x (mm)");
+  h_PDF_pion->GetXaxis()->CenterTitle();
+  h_PDF_pion->GetXaxis()->SetTitleOffset(1.25);
+  h_PDF_pion->GetXaxis()->SetNdivisions(505);
 
-  string outputfile = Form("../figures/masshypo_dist/mass_hypotheses_%s.pdf",particle.second.c_str());
+  h_PDF_pion->GetYaxis()->SetTitle("out_y (mm)");
+  h_PDF_pion->GetYaxis()->CenterTitle();
+  h_PDF_pion->GetYaxis()->SetTitleOffset(1.25);
+  h_PDF_pion->GetYaxis()->SetNdivisions(505);
+
+  h_PDF_pion->Draw("colz");
+
+  c_PDF->cd(2);
+  h_PDF_kaon->SetTitle("K+ mass hypotheses");
+  h_PDF_kaon->SetStats(0);
+
+  h_PDF_kaon->GetXaxis()->SetTitle("out_x (mm)");
+  h_PDF_kaon->GetXaxis()->CenterTitle();
+  h_PDF_kaon->GetXaxis()->SetTitleOffset(1.25);
+  h_PDF_kaon->GetXaxis()->SetNdivisions(505);
+
+  h_PDF_kaon->GetYaxis()->SetTitle("out_y (mm)");
+  h_PDF_kaon->GetYaxis()->CenterTitle();
+  h_PDF_kaon->GetYaxis()->SetTitleOffset(1.25);
+  h_PDF_kaon->GetYaxis()->SetNdivisions(505);
+
+  h_PDF_kaon->Draw("colz");
+
+  c_PDF->cd(3);
+  h_PDF_proton->SetTitle("p mass hypotheses");
+  h_PDF_proton->SetStats(0);
+
+  h_PDF_proton->GetXaxis()->SetTitle("out_x (mm)");
+  h_PDF_proton->GetXaxis()->CenterTitle();
+  h_PDF_proton->GetXaxis()->SetTitleOffset(1.25);
+  h_PDF_proton->GetXaxis()->SetNdivisions(505);
+
+  h_PDF_proton->GetYaxis()->SetTitle("out_y (mm)");
+  h_PDF_proton->GetYaxis()->CenterTitle();
+  h_PDF_proton->GetYaxis()->SetTitleOffset(1.25);
+  h_PDF_proton->GetYaxis()->SetNdivisions(505);
+
+  h_PDF_proton->Draw("colz");
+
+  string outputfile = "../figures/masshypo_dist/mass_hypotheses.pdf";
   cout << "save plot to: " << outputfile.c_str() << endl;
   c_PDF->SaveAs(outputfile.c_str());
+
+  TCanvas *c_Identified = new TCanvas("c_Identified","c_Identified",10,10,800,800);
+  c_Identified->cd();
+  c_Identified->cd()->SetLeftMargin(0.15);
+  c_Identified->cd()->SetBottomMargin(0.15);
+  c_Identified->cd()->SetGrid(0,0);
+  c_Identified->cd()->SetTicks(1,1);
+
+  h_PDF_pion->Draw("colz");
+  h_PDF_kaon->Draw("same");
+  h_PDF_proton->Draw("same");
+
+  outputfile = "../figures/masshypo_dist/mass_hypotheses_identify.pdf";
+  cout << "save plot to: " << outputfile.c_str() << endl;
+  c_Identified->SaveAs(outputfile.c_str());
 }
